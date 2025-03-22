b69c3996dad9859661e1be38d8e679570cdb94ae
Matthew Holman <mholman000@gmail.com>
2025-03-22 10:05:15 +0100

    Fix timer test

--- test/controllers/timer_controller_test.dart
+++ test/controllers/timer_controller_test.dart
@@ -4,11 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:lagom_lands/controllers/timer_controller.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   late TimerController timerController;
   late AnimationController animationController;
-  bool timerCompleted = false;
+  late bool timerCompleted;
 
   setUp(() {
+    timerCompleted = false;
     animationController = AnimationController(
       vsync: const TestVSync(),
       duration: const Duration(seconds: 5),
@@ -27,20 +30,43 @@ void main() {
 
   test('Timer counts down and completes correctly', () async {
     timerController.startTimer();
-    expect(timerController.remainingTime, 2);
+    expect(timerController.remainingTime, 5);
 
-    await Future.delayed(const Duration(seconds: 3));
-    expect(timerController.remainingTime, 0);
+    await Future.delayed(const Duration(seconds: 6));
+    // ensure callback is called
     expect(timerCompleted, true);
+    //ensure timer is reset
+    expect(timerController.remainingTime, 5);
   });
 
-  test('Timer stops and resets correctly', () async {
+  test('Timer stops and resets correctly if cancelled', () async {
     timerController.startTimer();
     await Future.delayed(const Duration(seconds: 2));
 
     timerController.stopTimer();
     expect(timerController.remainingTime, 5);
     expect(animationController.value, 0);
+  });
+
+  test('Timer handles rapid start-stop actions gracefully', () async {
+    // Rapidly start and stop the timer multiple times
+    for (int i = 0; i < 5; i++) {
+      timerController.startTimer();
+      await Future.delayed(const Duration(milliseconds: 200));
+      timerController.stopTimer();
+      await Future.delayed(const Duration(milliseconds: 100));
+    }
+
+    // Check if the timer is correctly reset and no callbacks were wrongly triggered
+    expect(timerController.remainingTime, 5);
+    expect(animationController.value, 0);
+    expect(timerCompleted, false);
+
+    // Finally, start the timer fully to completion to confirm it still works normally
+    timerController.startTimer();
+    await Future.delayed(const Duration(seconds: 6));
+    expect(timerCompleted, true);
+    expect(timerController.remainingTime, 5);
   });
 }
 
